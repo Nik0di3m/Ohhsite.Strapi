2f4ff929e820b1efda914d9c6869a75fc9116431
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-06-16 13:05:17 +0200

    strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40adb47bffb21f70ba3c8705a68773ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f48aeb2e-8c0e-4ef1-890c-53773e50f62d'
+};
